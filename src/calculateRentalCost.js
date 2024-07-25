1a03e0aa54326bc51746b875419947d196aeb894
Moshin Ilya <kingofexig@gmail.com>
2024-07-25 15:59:34 +0300

    write code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,27 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const prisePerDay = 40;
+  const day = Number(days);
+  let cost = day * prisePerDay;
+  const discountTriday = 20;
+  const discountWeek = 50;
+  const week = 7;
+  const triday = 3;
+
+  if (day >= week) {
+    cost -= discountWeek;
+
+    return cost;
+  }
+
+  if (day >= triday) {
+    cost -= discountTriday;
+
+    return cost;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
